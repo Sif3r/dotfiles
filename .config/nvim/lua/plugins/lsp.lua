3ea66f7589d5a4355e6f1246ddc55e1553f44604
Aurélien Aoustin <62019680+Sif3r@users.noreply.github.com>
2026-01-06 09:25:05 +0100

    refactor(nvim): remove lazyload lsp

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -2,7 +2,7 @@ return {
     {
         'VonHeikemen/lsp-zero.nvim',
         branch = 'v3.x',
-        event = { "BufReadPre", "BufNewFile" },
+        -- event = { "BufReadPre", "BufNewFile" },
         dependencies = {
             {'williamboman/mason.nvim'},
             {'williamboman/mason-lspconfig.nvim'},
--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,6 +1,6 @@
 return {
     "nvim-treesitter/nvim-treesitter",
-    branch = "main", -- 'main' is the rewrite/experimental branch (new API)
+    branch = "main",
     lazy = false,
     build = ":TSUpdate",
     config = function()
